c1fe1156936bd7fa068b74803f9c09c012f0844d
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-02-19 00:03:12 +0000

    Added

---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Dylan Mutabazi All rights reserved
+//
+// Created by Dylan Mutabazi
+// Created Feb 2025
+// This program runs basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "9+2 = " << (9 + 2) << std::endl;
+    std::cout << "5-2 = " << (5 - 2) << std::endl;
+    std::cout << "10/2 = " << (10 / 2) << std::endl;
+    std::cout << "10 x 2 = " << (10 * 2) << std::endl;
+    std::cout << "6 ^ 2 = " << pow(6 , 2) << std::endl;
+}
